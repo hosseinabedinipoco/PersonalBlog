6fb0b6fbf60f40f621b2f6f121f0b1f140bf1bc9
Hossein <hosseinabedinipoco@gmail.com>
2024-09-05 20:08:24 +0330

    create Article model

--- myBlog/blog/models.py
+++ myBlog/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    date = models.DateField()
+    view = models.SmallIntegerField(default=0)
+    view_user = models.ManyToManyField(User)
+    wirter = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100, null=False)
+    user = models.ManyToManyField(User)
+    article = models.OneToOneField(Article)    
