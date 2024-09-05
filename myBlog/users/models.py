2d45ce11832a4f5b4bec98dbc78ba90a0c8aa8ef
Hossein <hosseinabedinipoco@gmail.com>
2024-09-05 19:52:51 +0330

    create user model

--- myBlog/users/models.py
+++ myBlog/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True, null=False)
+    password = models.CharField(max_length=20, null=False)
+    is_Admin = models.BooleanField(default=False)
+    
