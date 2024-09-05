95e21ff74b78d45cd283f5d5b1c453fd90b8f442
Hossein <hosseinabedinipoco@gmail.com>
2024-09-05 20:16:20 +0330

    create comment model

--- myBlog/blog/models.py
+++ myBlog/blog/models.py
@@ -5,10 +5,10 @@ class Article(models.Model):
     title = models.CharField(max_length=50, null=False)
     date = models.DateField()
     view = models.SmallIntegerField(default=0)
-    view_user = models.ManyToManyField(User)
-    wirter = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    view_user = models.ManyToManyField(User, related_name='articles_viewed')
+    wirter = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='articles_written')
 
 class Comment(models.Model):
     content = models.CharField(max_length=100, null=False)
     user = models.ManyToManyField(User)
-    article = models.OneToOneField(Article)    
+    article = models.OneToOneField(Article, on_delete=models.CASCADE)    
